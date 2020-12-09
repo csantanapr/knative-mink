b2d92dd9f6cf61ff3b24d722cbf612863532f0f3
Carlos Santana <csantana23@gmail.com>
2020-12-09 13:10:40 -0500

    add demo script

---
+++ demo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+echo -e "\033[0;92m 🍿 Knative starting... \033[0m"
+STARTTIME=$(date +%s)
+curl -sL https://raw.githubusercontent.com/csantanapr/knative-kind/master/01-kind.sh | bash
+curl -sL https://raw.githubusercontent.com/csantanapr/knative-mink/main/02-mink.sh | bash
+DURATION=$(($(date +%s) - $STARTTIME))
+echo "kubectl get ksvc,broker,trigger"
+kubectl -n default get ksvc,broker,trigger
+echo -e "\033[0;92m 🚀 Knative setup with samples took: $(($DURATION / 60))m$(($DURATION % 60))s \033[0m"
+echo -e "\033[0;92m 🎉 Now have some fun with Serverless and Event Driven Apps \033[0m"
